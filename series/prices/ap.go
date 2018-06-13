f8a2d020a65e5212f04aa319ea93a9b5b9d4c91b
henry <dev@hwr.io>
2018-06-13 08:05:49 -0400

    create factory functions for prices/ap and prices/su - somehow they didn't get created

--- series/prices/ap.go
+++ series/prices/ap.go
@@ -5,3 +5,13 @@ import "github.com/henryroyal/bls/series"
 type AveragePriceData struct {
 	series.Dataset
 }
+
+func NewAveragePriceData() (*AveragePriceData) {
+	return &AveragePriceData{
+		series.Dataset{
+			Name:    "Average Prices",
+			Symbol:  "ap",
+			BaseURL: series.BaseURL,
+		},
+	}
+}
--- series/prices/cu.go
+++ series/prices/cu.go
@@ -5,3 +5,13 @@ import "github.com/henryroyal/bls/series"
 type ConsumerPriceIndexAllUrban struct {
 	series.Dataset
 }
+
+func NewConsumerPriceIndexAllUrban() (*ConsumerPriceIndexAllUrban) {
+	return &ConsumerPriceIndexAllUrban{
+		series.Dataset{
+			Name:    "Consumer Price Index - All Urban Consumers",
+			Symbol:  "cu",
+			BaseURL: series.BaseURL,
+		},
+	}
+}
