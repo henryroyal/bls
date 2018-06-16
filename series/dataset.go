1900f0373e30e4c6c7a756cc816736bb4fd59bc8
henry <dev@hwr.io>
2018-06-15 22:03:49 -0400

    add DatasetDescriptor interface, implement DatasetFiles - which makes an http request and formats all dataset filenames as a slice of strings

--- series/dataset.go
+++ series/dataset.go
@@ -2,13 +2,17 @@ package series
 
 import (
 	"fmt"
+	"net/http"
+	"github.com/PuerkitoBio/goquery"
+	"path"
 )
 
 const BaseURL string = `https://download.bls.gov/pub/time.series/`
 
-type DatasetFetcher interface {
-	DocumentationURL()
-	SeriesIndexURL()
+type DatasetDescriptor interface {
+	String() string
+	DatasetURL() string
+	DatasetFiles() ([]string, error)
 }
 
 type Dataset struct {
@@ -17,14 +21,51 @@ type Dataset struct {
 	BaseURL string
 }
 
+func (self *Dataset) getHTMLDocument(url string) (*goquery.Document, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return nil, fmt.Errorf(
+			"getHTMLDocument: http response code error: %d %s",
+			resp.StatusCode,
+			resp.Status,
+		)
+	}
+
+	index, err := goquery.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return index, nil
+}
+
+func (self *Dataset) DatasetFiles() ([]string, error) {
+
+	var files []string
+
+	index, err := self.getHTMLDocument(self.DatasetURL())
+	if err != nil {
+		return nil, err
+	}
+
+	index.Find("html body pre a").Each(func(_ int, li *goquery.Selection) {
+		if val, ok := li.Attr("href"); ok {
+			_, file := path.Split(val)
+			files = append(files, file)
+		}
+	})
+	return files, nil
+}
+
 func (self *Dataset) DatasetURL() string {
-	return (self.BaseURL + self.Symbol)
+	return self.BaseURL + self.Symbol + "/"
 }
 
-func (self *Dataset) DocumentationURL() string {
-	return (self.BaseURL + self.Symbol + fmt.Sprintf("%v.txt", self.Symbol))
-}
-
-func (self *Dataset) SeriesIndexURL() string {
-	return (self.BaseURL + self.Symbol + fmt.Sprintf("%v.series", self.Symbol))
+func (self *Dataset) String() string {
+	return self.Symbol
 }
